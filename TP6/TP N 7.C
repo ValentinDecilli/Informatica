5b3d73d8dc45752c111ac0d74e307ba45b860c24
ValentinDecilli <decillivalentin@gmail.com>
2023-08-25 19:39:01 -0300

    actualizados

--- TP6/TP N 7.C
+++ TP6/TP N 7.C
@@ -2,54 +2,57 @@
 #include <stdio.h>
 #include <math.h>
 
-//Realizar una función que reciba como argumentos 2 matrices de número reales de 3x3 y obtenga el producto de las 2 matrices. 
+// Realizar una función que reciba como argumentos 2 matrices de número reales de 3x3 y obtenga el producto de las 2 matrices.
 
 int matriz1[3][3], matriz2[3][3];
 int flag = 0;
+int producto, puntero, puntero_2;
+int matriztotal[3][3];
+void matricez()
+{
 
-int main(){
+    if (flag == 0)
+    {
 
-    	printf("ingrese los valores de su primer matriz:\n");
-        int matriz_1[3][3] = matricez();
-	    printf("ingrese los valores de su segunda matriz:\n");
-        int matriz_2[3][3] = matricez();
+        printf("ingrese los valores de su primer matriz:\n");
+        for (int j = 0; j < 3; j++)
+        {
+            for (int i = 0; i < 3; i++)
+            {
 
-        int valor = void operacion(*matriz_1,*matriz_2);
-}
-
-
-
-void matricez(){
-
-    if(flag == 0){
-        for(int j = 0 ; j < 3 ; j++){
-            
-            for(int i = 0 ; i < 3 ; i++){
-                
                 scanf("%d", &matriz1[j][i]);
-                
-                printf ("valor de su primera matriz %d[%d][%d]\n",matriz1[j][i],j,i);
+
+                printf("valor de su primera matriz %d[%d][%d]\n", matriz1[j][i], j, i);
             }
         }
-        flag == 1;	
-        return matriz1;
+
+        flag = 1;
     }
-    else{
-        for(int j = 0 ; j < 3 ; j++){
-            
-            for(int i = 0 ; i < 3 ; i++){
-                
+
+    else
+    {
+        printf("ingrese los valores de su segunda matriz:\n");
+        for (int j = 0; j < 3; j++)
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+
                 scanf("%d", &matriz2[j][i]);
-                
-                printf ("valor de su primera matriz %d[%d][%d]\n",matriz2[j][i],j,i);
+
+                printf("valor de su segunda matriz %d[%d][%d]\n", matriz2[j][i], j, i);
             }
         }
     }
-    return matriz2;
 }
 
 
-void operacion(matriz_1[],matriz_2[]){
+int main()
+{
 
-    return 0;
+    matricez();
+    matricez();
+    operacion(*matriz1,*matriz2);
+
 }
+
